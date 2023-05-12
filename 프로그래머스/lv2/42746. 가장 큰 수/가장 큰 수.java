a0dd359553bdcb47de77e053e54541129b92d8e5
JunSeong, Bae <gentlmanjun@naver.com>
2023-05-12 16:47:14 +0900

    [level 2] Title: 가장 큰 수, Time: 2.88 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,33 +1,38 @@
 import java.util.Arrays;
 
 class Solution {
-    public int getFst (int num) {
-        while(num >= 10) {
-            num /= 10;
-        }
-        return num;
-    }
-    public int getRes (int o1, int o2) {
-        int num = o2;
-        int tmp = o1;
-        while (num > 0) {
-            tmp *= 10;
-            num /= 10;
-        }
-        tmp += o2;
-        return tmp;
-    }
+    // public int getFst (int num) {
+    //     while(num >= 10) {
+    //         num /= 10;
+    //     }
+    //     return num;
+    // }
+    // public int getRes (int o1, int o2) {
+    //     int num = o2;
+    //     int tmp = o1;
+    //     while (num > 0) {
+    //         tmp *= 10;
+    //         num /= 10;
+    //     }
+    //     tmp += o2;
+    //     return tmp;
+    // }
     public String solution(int[] numbers) {
         Integer[] numArr = new Integer[numbers.length];
         int idx = 0;
         for(int num : numbers)
             numArr[idx++] = num;
         Arrays.sort(numArr, (o1, o2) -> {
-            if(getFst(o1) != getFst(o2))
-                return getFst(o2) - getFst(o1);
-            int tmp1 = getRes(o1, o2);
-            int tmp2 = getRes(o2, o1);
-            return tmp2 - tmp1;
+            // int fst1 = getFst(o1);
+            // int fst2 = getFst(o2);
+            // if(fst1 != fst2)
+            //     return fst2 - fst1;
+            // int tmp1 = getRes(o1, o2);
+            // int tmp2 = getRes(o2, o1);
+            // return tmp2 - tmp1;
+            String s1 = Integer.toString(o1);
+            String s2 = Integer.toString(o2);
+            return Integer.compare(Integer.parseInt(s2 + s1), Integer.parseInt(s1 + s2));
         });
         String answer = "";
         for(int num : numArr) {
