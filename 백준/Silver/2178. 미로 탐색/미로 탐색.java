5aa9900858ab84082a27688772c54b252e3f35f3
JunSeong, Bae <gentlmanjun@naver.com>
2023-09-16 09:34:18 +0900

    [Silver I] Title: 미로 탐색, Time: 100 ms, Memory: 12380 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,88 +1,70 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
-//미로 탐색
 public class Main {
-	static class idx {
-		int n, m;
-
-		public idx(int n, int m) {
-			this.n = n;
-			this.m = m;
-		}
-	}
-
-	static int N;
-	static int M;
-	static boolean[][] map;
-	static boolean[][] visited;
-	static int[] dx = { 0, 0, 1, -1 };
-	static int[] dy = { 1, -1, 0, 0 };
-	static int ans;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] split = br.readLine().split(" ");
-		N = Integer.parseInt(split[0]);
-		M = Integer.parseInt(split[1]);
-		map = new boolean[N][M];
-		visited = new boolean[N][M];
-
-		for (int i = 0; i < N; i++) {
-			split = br.readLine().split("");
-			for (int j = 0; j < M; j++) {
-				if (Integer.parseInt(split[j]) == 0)
-					map[i][j] = true;
-			}
-		}
-
-		ans = Integer.MAX_VALUE;
-
-		bfs();
-
-		if (ans > N * M)
-			ans = -1;
-		System.out.println(ans);
-	}
-
-	static void bfs() {
-		Queue<idx> queue = new LinkedList<idx>();
-		queue.add(new idx(0, 0));
-		int cnt = 1;
-
-		while (!queue.isEmpty()) {
-			int size = queue.size();
-			for (int time = 0; time < size; time++) {
-				idx now = queue.poll();
-				int n = now.n;
-				int m = now.m;
-
-				if (n == N - 1 && m == M - 1) {
-					if (ans > cnt)
-						ans = cnt;
-					return;
-				}
-
-				if (visited[n][m])
-					continue;
-				visited[n][m] = true;
-
-				for (int k = 0; k < 4; k++) {
-					int x = n + dx[k];
-					int y = m + dy[k];
-
-					if (x < 0 || y < 0 || x >= N || y >= M)
-						continue;
-					if (map[x][y] || visited[x][y])
-						continue;
-
-					queue.add(new idx(x, y));
-				}
-			}
-			cnt++;
-		}
-	}
+    public static void main(String args[]) throws IOException {
+        int[] dx = { 1, -1, 0, 0 };
+		int[] dy = { 0, 0, 1, -1 };
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        boolean[][] map = new boolean[N][M];
+        boolean[][] visited = new boolean[N][M];
+        
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for(int j = 0; j < M; j++) {
+                if(line.charAt(j) - '0' == 1) {
+                    map[i][j] = true;
+                }
+                visited[i][j] = false;
+            }
+        }
+        
+        Queue<Integer[]> queue = new LinkedList<>();
+        Integer[] node = new Integer[2];
+        node[0] = 0;
+        node[1] = 0;
+        queue.add(node);
+        
+        int ans = 0;
+        
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for(int s = 0; s < size; s++) {
+                node = queue.poll();
+                
+                if(node[0] == N-1 && node[1] == M-1) {
+                    ans++;
+                    bw.write(ans + "");
+                    bw.close();
+                    return;
+                }
+                
+                for(int i = 0; i < 4; i++) {
+                    int nx = node[0] + dx[i];
+                    int ny = node[1] + dy[i];
+                    
+                    if(nx < 0 || ny < 0 || nx >= N || ny >= M)
+                        continue;
+                    
+                    if(!visited[nx][ny] && map[nx][ny]) {
+                        visited[nx][ny] = true;
+                        Integer[]  next = new Integer[2];
+                        next[0] = nx;
+                        next[1] = ny;
+                        queue.add(next);
+                    }
+                }
+            }
+            ans++;
+        }
+    }
 }
