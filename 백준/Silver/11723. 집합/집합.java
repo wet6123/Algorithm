ee3ba312e451e0724cae55083f7541d168bba6af
JunSeong, Bae <gentlmanjun@naver.com>
2023-09-21 11:04:48 +0900

    [Silver V] Title: 집합, Time: 964 ms, Memory: 296636 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,48 +1,40 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int set = 0;
-		int M = Integer.parseInt(st.nextToken());
-		while (M-- > 0) {
-			st = new StringTokenizer(br.readLine());
-			String op = st.nextToken();
-			int num;
-
-			if (op.equals("add")) {
-				num = Integer.parseInt(st.nextToken());
-				set |= (1 << (num - 1));
-			} else if (op.equals("remove")) {
-				num = Integer.parseInt(st.nextToken());
-				set &= ~(1 << (num - 1));
-			} else if (op.equals("check")) {
-				num = Integer.parseInt(st.nextToken());
-				int re;
-				if ((set & (1 << (num - 1))) == (1 << (num - 1))) {
-					re = 1;
-				} else {
-					re = 0;
-				}
-				bw.write(re + "\n");
-			} else if(op.equals("toggle")) {
-				num = Integer.parseInt(st.nextToken());
-				set ^= (1 << (num - 1));
-			} else if(op.equals("all")) {
-				set |= (~0);
-			} else if(op.equals("empty")) {
-				set &= 0;
-			}
-		}
-		bw.close();
-	}
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int set = 0;
+        int M = Integer.parseInt(st.nextToken());
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            int num;
+            
+            if (op.equals("add")) {
+                num = Integer.parseInt(st.nextToken());
+                set |= (1 << (num - 1));
+            } else if (op.equals("remove")) {
+                num = Integer.parseInt(st.nextToken());
+                set &= ~(1 << (num - 1));
+            } else if (op.equals("check")) {
+                num = Integer.parseInt(st.nextToken());
+                if ((set & (1 << (num - 1))) == (1 << (num - 1)))
+                    bw.write("1\n");
+                else
+                    bw.write("0\n");
+            } else if (op.equals("toggle")) {
+                num = Integer.parseInt(st.nextToken());
+                set ^= (1 << (num - 1));
+            } else if (op.equals("all")) {
+                set |= (~0);
+            } else if (op.equals("empty")) {
+                set &= 0;
+            }
+        }
+        bw.close();
+    }
 }
