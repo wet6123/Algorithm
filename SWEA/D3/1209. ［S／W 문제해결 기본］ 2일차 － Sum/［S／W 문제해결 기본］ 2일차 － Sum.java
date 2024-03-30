d929cc904cebd5dadadaf461c0b2d20f59dff1b5
JunSeong, Bae <gentlmanjun@naver.com>
2024-03-30 19:53:45 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 261 ms, Memory: 111,720 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -1,42 +1,40 @@
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+//[S/W 문제해결 기본] 2일차 - Sum
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=10;
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-            sc.nextInt();
-            int[] sum = new int[202];
-            for(int i = 0; i < 100; i++)
-            {
-                for(int j = 0; j < 100; j++)
-                {
-                    int tmp = sc.nextInt();
-                    sum[j] += tmp;
-                    sum[100 + i] += tmp;
-                    if (i == j)
-                    {
-                        sum[200] += tmp;
-                    }
-                    if (i + j == 99)
-                    {
-                        sum[201] += tmp;
-                    }
-                }
-            }
-            int ans = 0;
-            for(int i = 0; i < 202; i++)
-            {
-                if (ans < sum[i])
-                    ans = sum[i];
-            }
-            System.out.println("#" + test_case + " " + ans);
+		for (int i = 0; i < 10; i++) {
+		int T = sc.nextInt();
+			int[][] arr = new int[100][100];
+			int max = Integer.MIN_VALUE;
+			int sum;
+			int diag1 = 0;
+			int diag2 = 0;
+			for (int j = 0; j < 100; j++) {
+				sum = 0;
+				for (int k = 0; k < 100; k++) {
+					arr[j][k] = sc.nextInt();
+					sum += arr[j][k];
+				}
+				if (sum > max)
+					max = sum;
+			}
+			for (int k = 0; k < 100; k++) {
+				diag1 += arr[k][k];
+				diag2 += arr[99 - k][99 - k];
+				sum = 0;
+				for (int j = 0; j < 100; j++) {
+					sum += arr[j][k];
+				}
+				if (sum > max)
+					max = sum;
+			}
+			if (diag1 > max)
+				max = diag1;
+			if (diag2 > max)
+				max = diag2;
+			System.out.printf("#%d %d\n",T,max);
 		}
 	}
 }
