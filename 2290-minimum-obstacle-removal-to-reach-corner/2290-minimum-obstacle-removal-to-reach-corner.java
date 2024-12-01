7103ba4fa53a38e9d4fa83eb87e3a16fed17c38a
JunSeong, Bae <gentlmanjun@naver.com>
2024-12-02 01:36:20 +0900

    Time: 133 ms (42.65%), Space: 83 MB (13.21%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.java
@@ -1,66 +1,48 @@
 class Solution {
     public int minimumObstacles(int[][] grid) {
-        // grid 사이즈 측정
-        // bfs 돌림
-        // 벽 만나면 큐에 벽 추가
-        // 도착 못하면 -> 큐에 있는 벽 없애면서 bfs
-        // 도착하면 정답 리턴
+        int m = grid.length;
+        int n = grid[0].length;
         int[] dx = {0, 0, 1, -1};
         int[] dy = {1, -1, 0, 0};
-
-        int ans = 0;
-        int x = grid[0].length;
-        int y = grid.length;
-
-        int[][] cnt_map = new int[y][x];
-        for(int tmp[]:cnt_map)
-            Arrays.fill(tmp, Integer.MAX_VALUE);
-
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {0, 0});
-        cnt_map[0][0] = 0;
-
-        while(true) {
-            while(q.size() > 0) {
-                int[] coor = q.remove();
-                int coor_y = coor[0];
-                int coor_x = coor[1];
-                int cnt = cnt_map[coor_y][coor_x];
-
-                for(int i = 0; i < 4; i++) {
-                    int cur_y = coor_y + dy[i];
-                    int cur_x = coor_x + dx[i];
-
-                    if (cur_x < 0 || cur_x >= x || cur_y < 0 || cur_y >= y)
-                        continue;
-
-                    if (grid[cur_y][cur_x] == 0) {
-                        if (cnt_map[cur_y][cur_x] > cnt) {
-                            cnt_map[cur_y][cur_x] = cnt;
-                            q.add(new int[] {cur_y, cur_x});
-                        }
-                    } else {
-                        if (cnt_map[cur_y][cur_x] > cnt + 1) {
-                            cnt_map[cur_y][cur_x] = cnt + 1;
-                            q.add(new int[] {cur_y, cur_x});
-                        }
-                    }
+        
+        // 거리 배열 초기화
+        int[][] distance = new int[m][n];
+        for (int[] row : distance) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        
+        // 우선순위 큐 사용 - [y, x, 제거한 장애물 수]
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        pq.offer(new int[]{0, 0, 0});
+        distance[0][0] = 0;
+        
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            int y = current[0], x = current[1], obstacles = current[2];
+            
+            // 이미 더 좋은 경로를 찾았다면 스킵
+            if (obstacles > distance[y][x]) continue;
+            
+            // 목적지에 도달했다면 결과 반환
+            if (y == m-1 && x == n-1) return obstacles;
+            
+            // 4방향 탐색
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                
+                if (ny < 0 || nx < 0 || ny >= m || nx >= n) continue;
+                
+                int newObstacles = obstacles + grid[ny][nx];
+                
+                // 더 적은 장애물을 제거하고 도달할 수 있다면 갱신
+                if (newObstacles < distance[ny][nx]) {
+                    distance[ny][nx] = newObstacles;
+                    pq.offer(new int[]{ny, nx, newObstacles});
                 }
             }
-            if (cnt_map[y - 1][x - 1] != Integer.MAX_VALUE)
-                break;
         }
-
-        // for(int i = 0; i < y; i++) {
-        //     for(int j = 0; j < x; j++) {
-        //         if (cnt_map[i][j] == Integer.MAX_VALUE)
-        //             System.out.print("x ");
-        //         else
-        //             System.out.print(cnt_map[i][j] + " ");
-        //     }
-        //     System.out.println();
-        // }
-
-        return cnt_map[y - 1][x - 1];
+        
+        return distance[m-1][n-1];
     }
 }
