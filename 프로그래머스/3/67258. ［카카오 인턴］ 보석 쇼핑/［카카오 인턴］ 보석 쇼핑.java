105ac0ac6839ea5fe9629ee0e9d86fcafc10f4b9
JunSeong, Bae <gentlmanjun@naver.com>
2024-12-06 02:16:18 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 55.64 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+
+class Solution {
+    static HashMap<String, Integer> map = new HashMap<>();
+    public int[] solution(String[] gems) {
+        int min = Integer.MAX_VALUE;
+        int ansStart = 0;
+        int ansEnd = 0;
+        
+        for(String key : gems) {
+            map.put(key, 0);
+        }
+        
+        int start = 0;
+        int end = 0;
+        int cnt = 0;
+        int size = map.size();
+        
+        while (end < gems.length) {
+            String key = gems[end];
+            if (map.get(key) == 0)
+                cnt++;
+            map.put(key, map.get(key) + 1);
+            while (cnt == size) {
+                if (end - start + 1 < min) {
+                    min = end - start + 1;
+                    ansStart = start;
+                    ansEnd = end;
+                    if (min == map.size())
+                        return new int[] {ansStart + 1, ansEnd + 1};
+                }
+                key = gems[start];
+                map.put(key, map.get(key) - 1);
+                if (map.get(key) <= 0)
+                    cnt--;
+                start++;
+            }
+            end++;
+        }
+        
+        return new int[] {ansStart + 1, ansEnd + 1};
+    }
+    
+    // public boolean checkPossible() {
+    //     for(Integer value : map.values()) {
+    //         if (value <= 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+}
