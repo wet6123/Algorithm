cac978b516611feb85226fe893a8984c52e4933f
JunSeong, Bae <gentlmanjun@naver.com>
2025-01-06 16:20:01 +0900

    [Gold IV] Title: 최단경로, Time: 760 ms, Memory: 120864 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,84 +1,80 @@
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.io.*;
 
-//최단경로
-
-class Node implements Comparable<Node>{
-	int idx;
-	int cost;
-
-	Node(int idx, int cost) {
-		this.idx = idx;
-		this.cost = cost;
-	}
-	
-	@Override
-	public int compareTo(Node o) {
-		return this.cost - o.cost;
-	}
+class Node {
+    int dest;
+    int weight;
+    
+    Node(int dest, int weight) {
+        this.dest = dest;
+        this.weight = weight;
+    } 
 }
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int V = sc.nextInt();
-		int E = sc.nextInt();
-
-		int start = sc.nextInt();
-
-		ArrayList[] map = new ArrayList[V + 1];
-
-		for (int i = 0; i < V + 1; i++) {
-			map[i] = new ArrayList<Node>();
-		}
-
-		for (int e = 0; e < E; e++) {
-			int u = sc.nextInt();
-			int v = sc.nextInt();
-			int w = sc.nextInt();
-
-			map[u].add(new Node(v, w));
-		}
-
-
-		int[] dist = new int[V + 1];
-
-		for (int i = 0; i < V + 1; i++) {
-			dist[i] = Integer.MAX_VALUE;
-		}
-
-		dist[start] = 0;
-
-		PriorityQueue<Node> pq = new PriorityQueue<Node>();
-		dist[start] = 0;
-		pq.add(new Node(start, 0));
-
-		while (!pq.isEmpty()) {
-			Node now = pq.poll();
-
-			if (now.cost > dist[now.idx])
-				continue;
-
-			int len = map[now.idx].size();
-			for (int i = 0; i < len; i++) {
-				Node next = (Node) map[now.idx].get(i);
-
-				if (dist[next.idx] > now.cost + next.cost) {
-					dist[next.idx] = now.cost + next.cost;
-					pq.add(new Node(next.idx, dist[next.idx]));
-				}
-			}
-
-		}
-		for (int i=1; i<=V; i++) {
-        	if (dist[i] == Integer.MAX_VALUE) {
-        		System.out.println("INF");
-        	}
-        	else {
-        		System.out.println(dist[i]);
-        	}
+    static ArrayList<Node>[] map;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken()) - 1;
+        
+        map = new ArrayList[V];
+        for(int i = 0; i < V; i++) {
+            map[i] = new ArrayList<>();
         }
-	}
+        
+        for(int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken()) - 1;
+            int v = Integer.parseInt(st.nextToken()) - 1;
+            int w = Integer.parseInt(st.nextToken());
+            map[u].add(new Node(v, w));
+        }
+        
+        int[] ans = Dijkstra(V, K);
+        
+        for(int num : ans) {
+            if (num == (int)1e8)
+                bw.write("INF\n");
+            else
+                bw.write(num + "\n");
+        }
+        bw.close();
+    }
+    
+    public static int[] Dijkstra(int numOfNodes, int start) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        int[] dest = new int[numOfNodes];
+        Arrays.fill(dest, (int)1e8);
+        dest[start] = 0;
+        pq.offer(new Node(start, 0));
+        
+        while(!pq.isEmpty()) {
+            Node now = pq.poll();
+            int idx = now.dest;
+            int weight = now.weight;
+            
+            if (weight > dest[idx]) continue;
+            
+            for(Node next : map[idx]) {
+                int nextIdx = next.dest;
+                int nextWeight = weight + next.weight;
+                if(nextWeight < dest[nextIdx]) {
+                    dest[nextIdx] = nextWeight;
+                    pq.offer(new Node(nextIdx, nextWeight));
+                }
+            }
+        }
+        
+        return dest;
+    }
 }
